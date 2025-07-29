83e0b8fbef37027a820bfcadedd488cb7f011755
MelanUras35 <167118991+MelanUras35@users.noreply.github.com>
2025-07-29 21:22:54 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import asyncio
+import logging
+import config
+from bot import client
 
+
+def start_bot():
+    try:
+        asyncio.run(client.start(config.TOKEN))
+    except KeyboardInterrupt:
+        logging.info("Bot kapatma isteği alındı. Kapatılıyor...")
+    except Exception:
+        logging.exception("Bot çalışırken hata oluştu:")
+    finally:
+        asyncio.run(client.close())
+        logging.info("Bot başarıyla durduruldu.")
+
+if __name__ == "__main__":
+    start_bot()
